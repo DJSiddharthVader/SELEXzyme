b1191dd51116034bd31da533a0d65ae47947b1b0
Sid Reed <sidreed34@gmail.com>
2020-10-13 13:51:18 -0700

    some commented out globals, cant really get globals to work

--- genetic_algorithm/main.go
+++ genetic_algorithm/main.go
@@ -1,11 +1,19 @@
 package main
 
 func main() {
+    /* // globals
+    var DNA_ALPHABET = []string{"A","G","C","T"}
+    var COMPLEMENT_MAP = map[rune]rune{'A':'T','T':'A','G':'C','C':'G'} //complementarty base pairs
+    MINIMUM_HAIRPIN_LENGTH_PROPORTION := 0.5
+    */
+    // examples
     var s Sequence
     s.seq = "AGCTCTCGGATCGATCGATATTTTAAAAAAA"
     s.fitness = 0.0
     var t Sequence
     t.seq = "TTTTAAAAAAAAGCTCTCGGATCGATCGATA"
     t.fitness = 0.0
+    // testing
     s.Complementarity(t)
+    s.ReverseComplement()
 }
