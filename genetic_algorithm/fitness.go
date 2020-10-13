6f4e3bd1afad64d2588c0b81448f6ccb0159ed9b
Sid Reed <sidreed34@gmail.com>
2020-10-13 13:50:17 -0700

    made reverse complement a Seqeunce method, changed HasHairpins() accordingly

--- genetic_algorithm/fitness.go
+++ genetic_algorithm/fitness.go
@@ -8,22 +8,22 @@ import(
 
 // ReverseComplement() reverses a DNA string and takes the complement
 // output: string
-func ReverseComplement(s string) string {
+func (s Sequence) ReverseComplement() string {
     var COMPLEMENT_MAP = map[rune]rune{'A':'T','T':'A','G':'C','C':'G'} //complementarty base pairs
     var result string
-    for _,v := range s {
+    for _,v := range s.seq {
         result = string(COMPLEMENT_MAP[v]) + result
     }
     return result
 }
-
 // HasHairpins() check if the sequence contains a palindrome that will cause hairpin binding in vivo
-// output: bool, true if it contains a pallindrome
-func (s Sequence) HasHairpins(MINIMUM_HAIRPIN_LENGTH_PROPORTION float64) bool{
-    minimumLength := int(float64(len(s.seq))*MINIMUM_HAIRPIN_LENGTH_PROPORTION)
+// output: int, number of hairpins
+func (s Sequence) HasHairpins(minimumnLengthProportion float64) int {
+    hairpins := 0
+    minimumLength := int(float64(len(s.seq))*minimumnLengthProportion)
     totalHalfwayPoint := int(len(s.seq)/2)
     var halfwayPoint int
-    var firstHalf string
+    var firstHalf,secondHalfReverse string
     for windowSize := minimumLength; windowSize < totalHalfwayPoint; windowSize++ {
         // check for pallendroms of length ranging from 2*minimumLength bp to half of the entire sequence
         // 0.5 times length of the pallendrome
@@ -31,16 +31,17 @@ func (s Sequence) HasHairpins(MINIMUM_HAIRPIN_LENGTH_PROPORTION float64) bool{
             // check every position if it is a pallendroms of length windowSize
             halfwayPoint = windowStart+windowSize // halfway point of the potential pallendrome
             firstHalf = s.seq[windowStart:halfwayPoint] // first half of the potential pallendrome
-            if firstHalf == ReverseComplement(s.seq[halfwayPoint:halfwayPoint+windowSize]) {
+            secondHalfReverse = s.ReverseComplement()
+            if firstHalf == secondHalfReverse[halfwayPoint:halfwayPoint+windowSize] {
                 // even length palindrome ACGG CCGT
-                return true
-            } else if firstHalf == ReverseComplement(s.seq[halfwayPoint:halfwayPoint+windowSize+1]) {
+                hairpins += 1
+            } else if firstHalf == secondHalfReverse[halfwayPoint:halfwayPoint+windowSize+1] {
                 // odd length palindrome ACGG T CCGT
-                return true
+                hairpins += 1
             }
         }
     }
-    return false
+    return hairpins
 }
 // Complementarity() returns BLAST score of sequence to target, higher is better for fitness
 // output: float64
@@ -60,12 +61,10 @@ func (s Sequence) Complementarity(target Sequence) float64{
 func (s Sequence) MeltingTemperature(targetTemp float64) float64{
     return float64(len(s.seq))
 }
-
 // Score() asseses the total fitness of a sequence
 // output: no output, just updates the fitness score
 func (s Sequence) Score(target string) Sequence {
     var t Sequence
     t.fitness = float64(len(s.seq)+len(target))
 }
-
 */
