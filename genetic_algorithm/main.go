6826e4daecea293b8d8720964b320dd94439ed8e
Sid Reed <sidreed34@gmail.com>
2020-11-01 20:18:34 -0800

    GA: sample simulation of the genetic algorithm, works

--- genetic_algorithm/main.go
+++ genetic_algorithm/main.go
@@ -1,19 +1,19 @@
 package main
 
+import(
+    "math/rand"
+)
+/* Parameter Restrictions
+   - mutation rate in [0,1]
+*/
+
+
 func main() {
-    /* // globals
-    var DNA_ALPHABET = []string{"A","G","C","T"}
-    var COMPLEMENT_MAP = map[rune]rune{'A':'T','T':'A','G':'C','C':'G'} //complementarty base pairs
-    MINIMUM_HAIRPIN_LENGTH_PROPORTION := 0.5
-    */
-    // examples
-    var s Sequence
-    s.seq = "AGCTCTCGGATCGATCGATATTTTAAAAAAA"
-    s.fitness = 0.0
-    var t Sequence
-    t.seq = "TTTTAAAAAAAAGCTCTCGGATCGATCGATA"
-    t.fitness = 0.0
-    // testing
-    s.Complementarity(t)
-    s.ReverseComplement()
+    rand.Seed(9)
+    lower := 50
+    upper := 500
+    size := 500
+    maxIterations := 500
+    lastGen := RunSimulation(size,lower,upper,maxIterations)
+    lastGen.WriteToFasta("test.fna")
 }
