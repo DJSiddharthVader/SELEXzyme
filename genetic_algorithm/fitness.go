acf01016e9eba6d92ada07b55b80ecb54debf372
Sid Reed <sidreed34@gmail.com>
2020-11-01 20:30:03 -0800

    GA: rethinking some stuff and replaced some things with constants

--- genetic_algorithm/fitness.go
+++ genetic_algorithm/fitness.go
@@ -2,29 +2,25 @@ package main
 
 import(
     "fmt"
-    "os/exec"
-    "strings"
 )
 
 // ReverseComplement() reverses a DNA string and takes the complement
 // output: string
-func (s Sequence) ReverseComplement() string {
-    var COMPLEMENT_MAP = map[rune]rune{'A':'T','T':'A','G':'C','C':'G'} //complementarty base pairs
+func (s Member) ReverseComplement() string {
     var result string
     for _,v := range s.seq {
-        result = string(COMPLEMENT_MAP[v]) + result
+        result = string(DNA_COMPLEMENTS[v]) + result
     }
     return result
 }
 // HasHairpins() check if the sequence contains a palindrome that will cause hairpin binding in vivo
 // output: int, number of hairpins
-func (s Sequence) HasHairpins(minimumnLengthProportion float64) int {
+func (s Member) HasHairpins() int {
     hairpins := 0
-    minimumLength := int(float64(len(s.seq))*minimumnLengthProportion)
     totalHalfwayPoint := int(len(s.seq)/2)
     var halfwayPoint int
     var firstHalf,secondHalfReverse string
-    for windowSize := minimumLength; windowSize < totalHalfwayPoint; windowSize++ {
+    for windowSize := MINIMUM_HAIRPIN_LENGTH; windowSize < totalHalfwayPoint; windowSize++ {
         // check for pallendroms of length ranging from 2*minimumLength bp to half of the entire sequence
         // 0.5 times length of the pallendrome
         for windowStart := 0; windowStart <= len(s.seq)-windowSize; windowStart++{
@@ -45,26 +41,48 @@ func (s Sequence) HasHairpins(minimumnLengthProportion float64) int {
 }
 // Complementarity() returns BLAST score of sequence to target, higher is better for fitness
 // output: float64
-func (s Sequence) Complementarity(target Sequence) float64{
-    BLASTN_PATH := "/usr/bin/blastn"
-    commandString := strings.Split(fmt.Sprintf("echo -e \">query\n%s\" >| query.fasta; echo -e \">suject\n%s\" >| subject.fasta; %s -query query.fasta -subject subject.fasta",s.seq,target.seq,BLASTN_PATH)," ")
-    command :=  exec.Command(commandString[0],commandString[1:]...)
-    fmt.Println(command.Run())
-    result,err := command.Output()
-    fmt.Println(result)
-    fmt.Println(err)
+func (s Member) Complementarity(target Member) float64 {
     return float64(len(s.seq)+len(target.seq))
 }
+// ScoreFitness() asseses the total fitness of a sequence
+// output: fitness score of s.seq
+func (s Member) ScoreFitness() float64 {
+    return float64(len(s.seq))
+}
 
+/*DEPRECIATED
 // MeltingTemperature() get the melting temperature of the sequence, returns abs difference to target
 // output: float64
 func (s Sequence) MeltingTemperature(targetTemp float64) float64 {
     return float64(len(s.seq))
 }
-// ScoreFitness() asseses the total fitness of a sequence
-// output: no output, just updates the fitness score
-func (s Sequence) ScoreFitness() {
-    var t Sequence
-    t.fitness = float64(len(s.seq))
+// HasHairpins() check if the sequence contains a palindrome that will cause hairpin binding in vivo
+// output: int, number of hairpins
+func (s Sequence) HasHairpins() int {
+    hairpins := 0
+    totalHalfwayPoint := int(len(s.seq)/2)
+    var halfwayPoint int
+    var firstHalf,secondHalfReverse string
+    for windowSize := MINIMUM_HAIRPIN_LENGTH; windowSize < totalHalfwayPoint; windowSize++ {
+        // check for pallendroms of length ranging from 2*minimumLength bp to half of the entire sequence
+        // 0.5 times length of the pallendrome
+        for windowStart := 0; windowStart <= len(s.seq)-windowSize; windowStart++{
+            // check every position if it is a pallendroms of length windowSize
+            halfwayPoint = windowStart+windowSize // halfway point of the potential pallendrome
+            firstHalf = s.seq[windowStart:halfwayPoint] // first half of the potential pallendrome
+            secondHalfReverse = s.ReverseComplement()
+            if firstHalf == secondHalfReverse[halfwayPoint:halfwayPoint+windowSize] {
+                // even length palindrome ACGG CCGT
+                hairpins += 1
+            } else if firstHalf == secondHalfReverse[halfwayPoint:halfwayPoint+windowSize+1] {
+                // odd length palindrome ACGG T CCGT
+                hairpins += 1
+            }
+        }
+    }
+    return hairpins
 }
-
+*/
+func newline() {
+    fmt.Println()
+}
