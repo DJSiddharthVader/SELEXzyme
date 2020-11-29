8a1abe76f740f58d965ab0c94b514b10fd1da304
Sid Reed <sidreed34@gmail.com>
2020-11-29 06:07:52 -0800

    changed SW matrix to have -1 insteand of -2 mismatch penalty

--- genetic_algorithm/data.go
+++ genetic_algorithm/data.go
@@ -1,21 +1,24 @@
 package main
 
 import(
+    "math"
     "github.com/biogo/biogo/alphabet"
     "github.com/biogo/biogo/align"
 )
 
 //Constants
+var INF = math.Inf(8)
+var NINF = math.Inf(-8)
 var DNA_ALPHABET = [4]rune{'A','C','G','T'}
 var DNA_COMPLEMENTS = map[rune]rune{'A':'T','C':'G','G':'C','T':'A'}
 var ALPHABET = alphabet.DNAgapped //alphabet for sequences
 var SW_MATRIX = align.SWAffine { //alignment matrix for SW, example from biogo docs
-        Matrix:  [][]int{        //       -   A   C   G   T
-            {0, -1, -1, -1, -1}, // -     0  -1  -1  -1  -1
-            {-1, 1, -2, -2, -2}, // A    -1   1  -2  -2  -2
-            {-1, -2, 1, -2, -2}, // C    -1  -2   1  -2  -2
-            {-1, -2, -2, 1, -2}, // G    -1  -2  -2   1  -2
-            {-1, -2, -2, -2, 1}, // T    -1  -2  -2  -2   1
+        Matrix:  [][]int{         //       -   A   C   G   T
+            {0,  -1, -1, -1, -1}, // -     0  -1  -1  -1  -1
+            {-1,  1, -1, -1, -1}, // A    -1   1  -1  -1  -1
+            {-1, -1,  1, -1, -1}, // C    -1  -1   1  -1  -1
+            {-1, -1, -1,  1, -1}, // G    -1  -1  -1   1  -1
+            {-1, -1, -1, -1,  1}, // T    -1  -1  -1  -1   1
         }, GapOpen: -5, //gap opening penalty
 }
 
